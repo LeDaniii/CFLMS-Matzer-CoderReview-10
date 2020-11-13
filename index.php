8a71a9c6326590e15ab2444bce3df18144d5cc74
LeDaniii <danielmatzer@gmail.com>
2020-11-13 18:22:08 +0100

    db works

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+// require_once("../phpDay3/php/component.php");
+require_once("../CF-CR10/php/db.php");
+// require_once("../phpDay3/php/operation.php");
+
+createdb();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="Description" content="Enter your description here" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Title</title>
+</head>
+
+<body>
+    <div class="container-fluid text-center bg-dark">
+        <h1 class="py-4 text-light"><i class="fas fa-book-reader"></i> MediaWerk Libary</h1>
+        <p class="py-2 text-light">Need something? We have it! maybe</p>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
--- php/db.php
+++ php/db.php
@@ -18,54 +18,85 @@ function createdb(){
     $sql = "create database if not exists $dbname";
 
     //  ##### Not necessary anymore #####
-     if(mysqli_query($con, $sql)){
-        echo "Hallo";
-    } else {
-        echo "cannnot create table!" . mysqli_error($con);
-    }
+    //  if(mysqli_query($con, $sql)){
+    //     echo "Hallo";
+    // } else {
+    //     echo "cannnot create table!" . mysqli_error($con);
+    // }
 
     if(mysqli_query($con, $sql)) {
         $con = mysqli_connect($servername, $username, $password, $dbname);
 
     $sql = "
-        create table if not exists media(
-            id int auto_increment primary key,
-            meal_img varchar(100),
-            meal_name varchar(30) not null,
-            meal_ingredients varchar(200),
-            meal_allergenes varchar(200),
-            meal_price float
+        create table if not exists author(
+            author_id int auto_increment primary key,
+            first_name varchar(30),
+            last_name varchar(30)
             ) 
     ";
 
+    // if(mysqli_query($con, $sql)){
+    //     echo "created table author!";
+    //     // return $con;
+    // } else {
+    //     echo "cannnot create table author!";
+    // }
+    
+    }
+
+    // ########################################
+    if(mysqli_query($con, $sql)) {
+        $con = mysqli_connect($servername, $username, $password, $dbname);
+
+    $sql = "
+        create table if not exists publisher(
+            publisher_id int auto_increment primary key,
+            publisher_name varchar(30),
+            publisher_adress varchar(30),
+            publisher_size varchar(10)
+            ) 
+    ";
+
+    // if(mysqli_query($con, $sql)){
+    //     echo "created table publisher!";
+    //     return $con;
+    // } else {
+    //     echo "cannnot create table publisher!";
+    // }
+    
+    }
+
+
+// #############################################
+    if(mysqli_query($con, $sql)) {
+        $con = mysqli_connect($servername, $username, $password, $dbname);
+
+    $sql = "
+        create table if not exists media(
+            media_id int auto_increment primary key,
+            media_title varchar(100),
+            media_img varchar(30),
+            media_isbn int,
+            media_description varchar(100),
+            media_publish_date date,
+            media_type varchar(10),
+            media_available varchar(20),
+            fk_author_id int,
+            fk_publisher_id int,
+            foreign key (fk_author_id) references author (author_id),
+            foreign key (fk_publisher_id) references publisher (publisher_id)
+            ) 
+    ";
     if(mysqli_query($con, $sql)){
+        // echo "created table media!";
         return $con;
     } else {
-        echo "cannnot create table 1!";
+        echo "cannnot create table media!" . mysqli_error($con);
     }
     
     }
 
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//    Note: there are three if statements because i don't know yet if there will be a problem with creating the foreign keys.
+//    Note2: I added on every column the name from the table to play it safe only for this time.
 
 }
